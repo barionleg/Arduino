6fcf1ad0cce75cb227ab691dbf57932b83f56b89
rizkihalasan <13515095@std.stei.itb.ac.id>
2018-04-10 16:57:57 +0700

    database arduino

---
+++ databaseori/databaseori.ino
@@ -0,0 +1,59 @@
+#include <EEPROM.h>
+
+#include <DB.h>
+#include <string.h>
+
+#include <Time.h>
+#include <TimeLib.h>
+
+#define MY_PINTU 1
+#define MY_USER 2
+
+DB db;
+struct Pintu {
+   char waktu[30];
+   char username[10];
+   char password[10];
+} pintu;
+
+struct User{
+  char username[10];
+  char email[20];
+} user;
+
+void databasePintu(){
+    sprintf(pintu.waktu, "10-04-2018 14:07:11");
+    sprintf(pintu.username, "leo");
+    sprintf(pintu.password, "masukaja");
+    Serial.println("Creating Table pintu...");  
+    db.create(MY_PINTU, sizeof(pintu));
+    db.open(MY_PINTU);
+    Serial.println("Creating records pintu");
+    db.append(DB_REC pintu);
+    db.read(1, DB_REC pintu);
+    Serial.print("waktu : "); Serial.println(pintu.waktu);
+    Serial.print("username : "); Serial.println(pintu.username);
+    Serial.print("password : "); Serial.println(pintu.password);
+}
+
+void databaseUser(){
+//  sprintf(user.username, "leo");
+//  sprintf(user.email, "leo@gmail.com");
+//  Serial.println("Creating Table user...");
+//  db.create(MY_USER, sizeof(user));
+  db.open(MY_USER);
+//  Serial.println("Creating records user");
+//  db.append(DB_REC user);
+  db.read(1, DB_REC user);
+  Serial.print("username : "); Serial.println(user.username);
+  Serial.print("email : "); Serial.println(user.email);
+}
+void setup() {
+  Serial.begin(9600);
+  databaseUser();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
